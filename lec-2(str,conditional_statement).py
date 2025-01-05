fc5c4f14d471d9fbf187e4939e59dd4dfa5c5bdd
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-01-05 18:14:56 +0530

    add lec 2

---
+++ lec-2(str,conditional_statement).py
@@ -0,0 +1,55 @@
+# String
+
+# str1 = 'string one'
+# str2 = "string two"
+# str3 = '''string three'''
+
+
+
+# # basics operation
+# str1 = "python"
+# str2 = "program"
+# finalStr = str1 + " "  + str2       #concatenation of string
+# print(finalStr)
+# print(len(finalStr))    #length of string
+
+
+
+# # indexing (start from 0)
+# str = "python program"
+# print(str[5])
+# # str[1] = 'K'    #assigning is not allowed
+
+
+
+# # slicing  
+# str = "python program"
+# print(str[2:5])     #2-include and 5-exclude
+# print(str[:5])      #same as str[0:5]
+# print(str[5:])      #same as str[5:len(str)]
+# print(str[-9:-1])   #negative indexing
+
+
+
+# #string function
+# str = "python program"
+# print(str.endswith("ram"))  #return true or false
+# print(str.capitalize())     #capitalize 1st latter
+# print(str.replace("python","javascript"))   #replace 'python' with 'javascript'
+# print(str.find("program"))   #return the 1st index of 1st occurrence
+# print(str.find("java"))     #return -1
+# print(str.count("p"))  #return the count of occurrence of sub string
+
+
+
+# conditional statement
+
+# age = int(input("Enter your age: "))  # Convert input to integer
+# if(age >= 16 and age <= 18):
+#     print("is teenage")
+#     print("not eligible for vote")
+# elif(age >= 18 and age <= 20):
+#     print("is teenage")
+#     print("is eligible for vote")
+# else:
+#     print("not a teenager")
