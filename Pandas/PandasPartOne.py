d8af2f79ee8298a55c7c3ce77062a14b979726c2
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-05-31 15:46:37 +0530

    publish the code

--- Pandas/PandasPartOne.py
+++ Pandas/PandasPartOne.py
@@ -1,83 +1,83 @@
 import pandas as pb
 
-# # reading the external files
-# df_csv = pb.read_csv('./Dataset/sales_data_sample.csv', encoding='latin1')
+# reading the external files
+df_csv = pb.read_csv('./Dataset/sales_data_sample.csv', encoding='latin1')
 
-# df_json = pb.read_json('./Dataset/sample_Data.json')
+df_json = pb.read_json('./Dataset/sample_Data.json')
 
-# print(df_csv)
-# print(df_json.head())
-
-
-# # saving the data frame
-# data = {
-#     "Name":["ram", 'sham', 'yash'],
-#     "Age":[20, 21, 22],
-#     "City":["kalyan", 'pune', 'mumbai']
-# }
-
-# df = pb.DataFrame(data)
-# print(df)
-
-# df.to_csv('./Dataset/new_data.csv', index=False)
+print(df_csv)
+print(df_json.head())
 
 
+# saving the data frame
+data = {
+    "Name":["ram", 'sham', 'yash'],
+    "Age":[20, 21, 22],
+    "City":["kalyan", 'pune', 'mumbai']
+}
 
+df = pb.DataFrame(data)
+print(df)
 
-
-# # .info() method
-# data = {
-#     "Name":["ram", 'sham', 'yash'],
-#     "Age":[20, 21, 22],
-#     "City":["kalyan", None, 'mumbai']
-# }
-
-# df = pb.DataFrame(data)
-# print(df.info())
-
-
-
-# # .describe() method
-# data = {
-#     "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
-#     "Age":[20, 21, 22, 25, 26, 23],
-#     "City":["kalyan", None, 'mumbai', 'pune', 'nagpur', 'banglore'],
-#     "salary":[10000, 20000, 30000, 40000, 50000, 60000]
-# }
-
-# df = pb.DataFrame(data)
-# print(df)
+df.to_csv('./Dataset/new_data.csv', index=False)
 
 
 
 
-# # shape and column
-# data = {
-#     "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
-#     "Age":[20, 21, 22, 25, 26, 23],
-#     "City":["kalyan", None, 'mumbai', 'pune', 'nagpur', 'banglore'],
-#     "salary":[10000, 20000, 30000, 40000, 50000, 60000]
-# }
 
-# df = pb.DataFrame(data)
-# print(f'shape: {df.shape}')
-# print(f'columns: {df.columns}')
+# .info() method
+data = {
+    "Name":["ram", 'sham', 'yash'],
+    "Age":[20, 21, 22],
+    "City":["kalyan", None, 'mumbai']
+}
 
-
-
-# # Accessing the rows and column
-# data = {
-#     "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
-#     "Age":[20, 21, 22, 25, 26, 23],
-#     "City":["kalyan", None, 'mumbai', 'pune', 'nagpur', 'banglore'],
-#     "salary":[10000, 20000, 30000, 40000, 50000, 60000]
-# }
-
-# df = pb.DataFrame(data)
-# print(df.iloc[0:4,0:3])
+df = pb.DataFrame(data)
+print(df.info())
 
 
 
+# .describe() method
+data = {
+    "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
+    "Age":[20, 21, 22, 25, 26, 23],
+    "City":["kalyan", None, 'mumbai', 'pune', 'nagpur', 'banglore'],
+    "salary":[10000, 20000, 30000, 40000, 50000, 60000]
+}
+
+df = pb.DataFrame(data)
+print(df)
+
+
+
+
+# shape and column
+data = {
+    "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
+    "Age":[20, 21, 22, 25, 26, 23],
+    "City":["kalyan", None, 'mumbai', 'pune', 'nagpur', 'banglore'],
+    "salary":[10000, 20000, 30000, 40000, 50000, 60000]
+}
+
+df = pb.DataFrame(data)
+print(f'shape: {df.shape}')
+print(f'columns: {df.columns}')
+
+
+
+# Accessing the rows and column
+data = {
+    "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
+    "Age":[20, 21, 22, 25, 26, 23],
+    "City":["kalyan", None, 'mumbai', 'pune', 'nagpur', 'banglore'],
+    "salary":[10000, 20000, 30000, 40000, 50000, 60000]
+}
+
+df = pb.DataFrame(data)
+print(df.iloc[0:4,0:3])
+
+
+# filtration
 data = {
     "Name":["ram", 'sham', 'yash', 'rohan', 'aditi', 'rohit'],
     "Age":[20, 21, 22, 25, 26, 23],
